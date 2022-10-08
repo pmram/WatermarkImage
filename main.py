6768d64e9c209c54dd41763ad0aba70f37e8b3a5
wolfsoad <pmramaia.engmec@gmail.com>
2022-10-08 18:22:04 +0100

    Update main.py to upgrade styles to customtkinter

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from tkinter import ttk
-
+import customtkinter
 from forms import Window
 
 # Basic GUI configuration
-root = Tk()
+root = customtkinter.CTk()
+root.set_appearance_mode("light")
 root.title("Watermarking tool")
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = customtkinter.CTkFrame(root)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
