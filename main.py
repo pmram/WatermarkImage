9a996f79e3228eeda9217ec11448049d18ede29d
wolfsoad <pmramaia.engmec@gmail.com>
2022-10-09 10:34:00 +0100

    Update main.py to upgrade window configuration

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from forms import Window
 root = customtkinter.CTk()
 root.set_appearance_mode("light")
 root.title("Watermarking tool")
+window_width = 550
+window_height = 400
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
 mainframe = customtkinter.CTkFrame(root)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
