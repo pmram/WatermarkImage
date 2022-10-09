2ccc529fc4b54b22ff76b6578b59ea50e2a4230e
wolfsoad <pmramaia.engmec@gmail.com>
2022-10-09 10:40:48 +0100

    Update imageoperator.py to change naming of saved file

--- imageoperator.py
+++ imageoperator.py
@@ -1,3 +1,5 @@
+import os.path
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -5,11 +7,11 @@ from PIL import ImageDraw
 
 class ImageProcessor:
 
-    def add_watermark(self, text, filename=None, pos_x=1, pos_y=1, size=0.1):
-        if filename:
+    def add_watermark(self, text, filepath=None, pos_x=1, pos_y=1, size=0.1):
+        if filepath:
             try:
-                with Image.open(filename) as im:
-                    print(filename, im.format, f"{im.size}x{im.mode}")
+                with Image.open(filepath) as im:
+                    print(filepath, im.format, f"{im.size}x{im.mode}")
                     watermark_image = im.copy()
                     text_height = int(im.size[0] * size)
                     draw = ImageDraw.Draw(watermark_image)
@@ -22,14 +24,21 @@ class ImageProcessor:
                     text_pos_y = im.size[1] * pos_y - (pos_y * text_h)
 
                     draw.text(xy=(text_pos_x, text_pos_y), text=text, fill=(0, 0, 0), font=font)
-                    self.save_image(watermark_image)
+                    self.save_image(watermark_image, filepath)
             except OSError:
                 pass
 
     @staticmethod
-    def save_image(image):
+    def save_image(image: Image, filepath: str) -> bool:
+        """
+        Saves new image in a file with original name plus "watermarked"
+        :param image: Watermarked image
+        :param filepath: String with original filepath
+        :return: True if success in saving file
+        """
         try:
-            image.save("watermarked_image.jpg")
+            filename = os.path.basename(filepath)
+            image.save(f"watermarked_{filename}")
             return True
         except PermissionError:
             return False
